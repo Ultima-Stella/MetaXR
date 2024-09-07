58642c6ee4d51b86249b5151a2c8191d9fb66ff5
Arda <macelik3434@gmail.com>
2024-09-07 10:10:09 +0300

    dasdaddsa

--- Assets/_Core/Scripts/Debug/RoomManagerDebugger.cs
+++ Assets/_Core/Scripts/Debug/RoomManagerDebugger.cs
@@ -2,34 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using GorillaZilla;
 using UnityEngine;
-using Utilities.XR;
+//using Utilities.XR;
 
-public class RoomManagerDebugger : MonoBehaviour
-{
+//public class RoomManagerDebugger : MonoBehaviour
+//{
 
-    public RoomManager roomManager;
-    private void OnDrawGizmos()
-    {
-        RenderGizmo();
-    }
-    private void Update()
-    {
-        RenderGizmo();
-    }
-    [ContextMenu("Render Gizmo")]
-    void RenderGizmo()
-    {
-        if (roomManager == null || !PlayerSettings.ShowDebug) return;
-        Grid grid = roomManager.GetComponent<Grid>();
-        List<Vector3> availableLocations = roomManager.GetAvailableSpawnLocations();
-        Gizmos.color = Color.green;
-        Vector3 cubeSize = grid.cellSize;
-        cubeSize.z = .01f;
-        foreach (Vector3 location in availableLocations)
-        {
-            XRGizmos.DrawCube(location, roomManager.transform.rotation, cubeSize, Color.green);
-        }
-    }
+//    public RoomManager roomManager;
+//    private void OnDrawGizmos()
+//    {
+//        RenderGizmo();
+//    }
+//    private void Update()
+//    {
+//        RenderGizmo();
+//    }
+//    [ContextMenu("Render Gizmo")]
+//    void RenderGizmo()
+//    {
+//        if (roomManager == null || !PlayerSettings.ShowDebug) return;
+//        Grid grid = roomManager.GetComponent<Grid>();
+//        List<Vector3> availableLocations = roomManager.GetAvailableSpawnLocations();
+//        Gizmos.color = Color.green;
+//        Vector3 cubeSize = grid.cellSize;
+//        cubeSize.z = .01f;
+//        foreach (Vector3 location in availableLocations)
+//        {
+//            XRGizmos.DrawCube(location, roomManager.transform.rotation, cubeSize, Color.green);
+//        }
+//    }
 
 
-}
+//}
