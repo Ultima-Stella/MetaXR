28a3d6db10aead42af36633b14d94ade817aa7f8
Arda <macelik3434@gmail.com>
2024-09-08 09:35:37 +0300

    değince kırmızı oluyor

---
+++ Assets/Marker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Marker : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Untagged"))
+        {
+            other.gameObject.tag = "Marked";
+            Rigidbody rb = other.GetComponent<Rigidbody>();
+            Debug.Log("Çarptı");
+            other.GetComponent<Renderer>().material.color = Color.red;
+        }
+
+    }
+    void Update()
+    {
+        
+    }
+}
